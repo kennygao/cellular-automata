5946a72cb56fcaafb5786e549165eec851088980
kennygao <kennygao@users.noreply.github.com>
2020-02-13 00:00:34 -0800

    Add a bit of Python boilerplate.

--- game-of-life/game-of-life.py
+++ game-of-life/game-of-life.py
@@ -1,4 +1,14 @@
-# conway's game of life
+"""
+This is a naive implementation of Conway's Game of Life.
 
-universe = [[0 for columns in range(5)] for rows in range(3)]
-print(f'{universe=}')
+The goal is to be pedagogical, not performant.
+"""
+
+
+def main():
+    universe = [[0 for columns in range(5)] for rows in range(3)]
+    print(f'{universe=}')
+
+
+if __name__ == '__main__':
+    main()
