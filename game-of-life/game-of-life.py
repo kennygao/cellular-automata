72579d4f73b28c3d5a24a15d921c4134f0dac92a
kennygao <kennygao@users.noreply.github.com>
2020-02-13 00:11:14 -0800

    Add basic visualization.

--- game-of-life/game-of-life.py
+++ game-of-life/game-of-life.py
@@ -5,9 +5,14 @@ The goal is to be pedagogical, not performant.
 """
 
 
+def display(universe):
+    return '\n'.join(''.join(str(cell) for cell in row) for row in universe)
+
+
 def main():
     universe = [[0 for columns in range(5)] for rows in range(3)]
     print(f'{universe=}')
+    print(display(universe))
 
 
 if __name__ == '__main__':
