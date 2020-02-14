5d5be329455d52a4d43c4d8b9a71e6cbe0baa9e9
kennygao <kennygao@users.noreply.github.com>
2020-02-14 01:31:50 -0800

    Minor refactoring.

--- game-of-life/game-of-life.py
+++ game-of-life/game-of-life.py
@@ -10,8 +10,12 @@ def display(universe):
     return "\n".join("".join(str(cell) for cell in row) for row in universe)
 
 
-def tick(universe):
-    return [[1 - cell for cell in row] for row in universe]
+def flip_cell(cell):
+    return 1 - cell
+
+
+def tick_universe(universe, tick_cell):
+    return [[tick_cell(cell) for cell in row] for row in universe]
 
 
 def main():
@@ -21,7 +25,7 @@ def main():
     print(display(universe))
 
     print("universe @ t[1]")
-    print(display(tick(universe)))
+    print(display(tick_universe(universe, flip_cell)))
 
 
 if __name__ == "__main__":
