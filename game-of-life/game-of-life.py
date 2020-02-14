25d71e08179d8a4da0f375bfaa9f9ef4b25c913e
kennygao <kennygao@users.noreply.github.com>
2020-02-14 01:04:08 -0800

    Add trivial tick function.

--- game-of-life/game-of-life.py
+++ game-of-life/game-of-life.py
@@ -10,10 +10,18 @@ def display(universe):
     return '\n'.join(''.join(str(cell) for cell in row) for row in universe)
 
 
+def tick(universe):
+    return universe
+
+
 def main():
     universe = [[random.randrange(2) for columns in range(5)] for rows in range(3)]
-    print(f'{universe=}')
+
+    print('universe @ t[0]')
     print(display(universe))
+
+    print('universe @ t[1]')
+    print(display(tick(universe)))
 
 
 if __name__ == '__main__':
